7fff2dc8a39032d4d1cd0e5c0cbf614728fe5db7
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-10-20 14:14:10 +0200

    feature: add build and save actions in nuke menu.

--- startup/menu.py
+++ startup/menu.py
@@ -1,4 +1,15 @@
-from silex_client.utils.context import Context
-import nuke
+from silex_client.core.context import Context
+from silex_client.utils.log import logger
+import traceback
 
-Context.get().ws_connection.start_multithreaded()
+actions = [item["name"] for item in Context.get().config.actions]
+
+def command(action_name):
+    Context.get().get_action(action_name).execute()
+
+def creat_menus():
+    for action in actions:
+        #nuke.error(action)
+        nuke.menu('Nuke').addCommand('SIlex/{}'.format(str(action)), lambda: command(action))
+
+creat_menus()
